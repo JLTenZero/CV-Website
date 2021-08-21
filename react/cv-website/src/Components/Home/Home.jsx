5b3f488e426cd357b7c4fef886fa6b4d8e83b2d2
Jordan Lewis <jordanlewis1@btinternet.com>
2021-08-21 15:55:33 +0100

    JL: Minor colour palette changes

--- react/cv-website/src/Components/Home/Home.jsx
+++ react/cv-website/src/Components/Home/Home.jsx
@@ -11,10 +11,15 @@ const Home = () => {
                     </header>
                     <h2>The colour palette for the website:</h2>
                     <div align="center">
+                        <h2>Primary</h2>
                         <p id="color1">Colour 1</p>
+                        <h2>Secondary</h2>
                         <p id="color2">Colour 2</p>
+                        <h2>Tertiary</h2>
                         <p id="color3">Colour 3</p>
+                        <h2>Accents</h2>
                         <p id="color4">Colour 4</p>
+                        <h2>???</h2>
                         <p id="color5">Colour 5</p>
                     </div>
                 </div>
